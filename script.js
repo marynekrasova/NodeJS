b129f913a5f48ad0d6ff3d5f6cef2cf9169c2eb7
maryia.nekrasova <manekrasova85@gmail.com>
2022-01-13 15:24:09 +0300

    NodeJS task1

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
-//Tasks
+//Task 1
+const colors = require('colors');
+
+function numPrime(min, max) {
+  let colorArr = ["green", "yellow", "red"];
+  if ((min > max) || (min < 0)) throw Error("Не верно задан диапазон");
+
+  let j = 0;
+  for (let i = 2; i <= max; i++) {
+    let text = colors[colorArr[j]] && colors[colorArr[j]](i);
+    console.log(text);
+    j = j === 2 ? 0 : j + 1;
+  }
+}
+
+numPrime(1, 100);
+
+
